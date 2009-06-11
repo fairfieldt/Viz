d4d0ebec1f53857c83f7f5ca6e0c99fcc0e68089
Tom Fairfield <antirush@gmail.com>
2009-06-11 18:27:22 -0500

    adding Scope.java

--- Scope.java
+++ Scope.java
@@ -4,6 +4,7 @@ public class Scope implements Drawable
 {
 	private String name;
 	private ArrayList<Variable> vars;
+	private ArrayList<Scope> scopes;
 
 	private int xPos = 50;
 	private int yPos = 50;
@@ -18,6 +19,7 @@ public class Scope implements Drawable
 	public Scope(String name, String color)
 	{
 		vars = new ArrayList<Variable>();
+		scopes = new ArrayList<Scope>();
 		this.name = name;
 		this.color = color;
 	}
@@ -29,9 +31,41 @@ public class Scope implements Drawable
 		this.vars.add(v);
 		currentVarPos+=v.getLength() + 10;
 	}
+	
+	public void addScope(Scope s)
+	{
+		scopes.add(s);
+	}
+	
+	public void setPosition(int xPos, int yPos)
+	{
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+	
+	public void setSize(int sizeX, int sizeY)
+	{
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+	}
+	
+	private void sizeScopes()
+	{
+		int subScopeXPos = xPos + 10;
+		int subScopeYPos = yPos + 100;
+		
+		for (Scope s : scopes)
+		{
+			s.setPosition(subScopeXPos, subScopeYPos);
+			s.setSize(sizeX - 20, sizeY-150 / (scopes.size()+1));
+		}		 		
+			
+	}
 
 	public void draw(XAALScripter scripter)
 	{
+		System.out.println("Drawing scope: " + name);
+		System.out.println("XPos: " + xPos + " YPos: " + yPos);
 		int captionLength = name.length() * 10;
 		scripter.addRectangle(xPos, yPos, sizeX, sizeY, color, hidden);
 		scripter.addRectangle(xPos, yPos-30, captionLength, 30, color, hidden);
@@ -40,6 +74,12 @@ public class Scope implements Drawable
 		for (Variable v : vars)
 		{
 			v.draw(scripter);
+		}
+		sizeScopes();
+		for (Scope s : scopes)
+		{
+			System.out.println("Drawing subScope");
+			s.draw(scripter);
 		}
 	}
 }
