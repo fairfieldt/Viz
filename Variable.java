4a700494e639aa8f3c82bbb13ac0c57ac4f904c0
Tom Fairfield <antirush@gmail.com>
2009-06-10 21:15:33 -0500

    initial commit

---
+++ Variable.java
@@ -0,0 +1,32 @@
+public class Variable implements Drawable
+{
+	private String name;
+	private int value;
+	private boolean isReference;
+	
+	private int xPos;
+	private int yPos;
+
+	public Variable(String name, int value, boolean isReference)
+	{
+		this.name = name;
+		this.value = value;
+		this.isReference = isReference;
+	}
+
+	pubic void draw(XAALScripter scripter)
+	{
+		if (this.isReference)
+		{
+			scripter.addCircle(xPos, yPos);
+		}
+		else
+		{
+			scripter.addRectangle(xPos, yPos);
+		}
+		
+		scripter.addText(name + " = " + value, xPos, yPos);
+	}
+}
+
+
