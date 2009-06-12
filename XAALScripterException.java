41c81c6e38a06bf65334e88994f1c722bbe8e3a7
unknown <Eric@.(none)>
2009-06-12 12:23:58 -0500

    Added support for dashed lines and variable line width. Additionally, created specialized exceptions for problems running the scripter.

---
+++ XAALScripterException.java
@@ -0,0 +1,10 @@
+
+public class XAALScripterException extends Exception {
+
+	public XAALScripterException(String string)
+	{
+		super(string);
+	}
+	private static final long serialVersionUID = 2162577875845825092L;
+
+}
