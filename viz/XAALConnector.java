1d643b5e78e70234a3286d8a513b0f79efc32448
Tom Fairfield <antirush@gmail.com>
2009-06-26 04:40:16 -0500

    1 down - movement works now

--- viz/XAALConnector.java
+++ viz/XAALConnector.java
@@ -298,7 +298,7 @@ public class XAALConnector {
 		 Variable v = varToVar.get(iv.getUUID());
 		 Array vArray = (Array)v;
 		 
-		 vArray.setElem(index, newValue);
+		 //vArray.setElem(index, newValue);
 		 
 		 vArray.addCopy(index);
 		 
