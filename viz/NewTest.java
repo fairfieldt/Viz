c8bcdf9a641f8623eb87bd78124d35baa76029c5
Tom Fairfield <antirush@gmail.com>
2009-06-27 02:21:40 -0500

    Revert "4/4  arrays totally work :):):)"
    This reverts commit 22d40ed8dcc104e5ad1e92280e18e70f14484647.

--- viz/NewTest.java
+++ viz/NewTest.java
@@ -12,7 +12,7 @@ public class NewTest
 
 			RandomizingVisitor rv = new RandomizingVisitor();
 				
-			//program.jjtAccept(rv, null);
+			program.jjtAccept(rv, null);
 		
 			System.out.println("Successfully Parsed");
 			System.out.println("________________\n");
@@ -37,7 +37,7 @@ public class NewTest
 			program.jjtAccept(iv, null);
 			System.out.println(Global.getFunction("foo").getParameters().size());
 			System.out.println(Global.getFunction("foo").getSymbolTable().getLocalVariables().size());
-			xc.draw("/home/fairfieldt/Documents/!real.xaal");
+			xc.draw("/home/fairfieldt/!real.xaal");
 						for (String line: program.getPseudocode())
 						{
 							System.out.println(line);
