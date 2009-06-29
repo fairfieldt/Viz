733db60ecd9b20fd7fdfebeb355c186f2587a4ad
unknown <Eric@.(none)>
2009-06-28 19:09:07 -0500

    RefTest2.java

---
+++ viz/RefTest2.java
@@ -0,0 +1,65 @@
+package viz;
+import Interpreter.*;
+import java.io.*;
+
+public class RefTest2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Starting");
+		Global.InterpreterType = InterpreterTypes.BY_REFERENCE;
+		BufferedReader br = null;
+		try
+		{
+			br = new BufferedReader(new FileReader(args[0]));
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
+		VizParser parser = new VizParser(br);
+		try
+		{
+			ASTProgram program = (ASTProgram)parser.program();
+
+			RandomizingVisitor rv = new RandomizingVisitor();
+				
+			//program.jjtAccept(rv, null);
+		
+			System.out.println("Successfully Parsed");
+			System.out.println("________________\n");
+			
+			program.dump("");
+			program.buildCode();
+			System.out.println("Built code");
+			
+			//program.dump("");
+			XAALConnector xc = new XAALConnector(program.getPseudocode(), "foo");
+		
+			for (String s : program.getPseudocode())
+			{
+				System.out.println(s);
+			}
+			System.out.println("\n\n Testing Interpret Visitor");
+			
+			QuestionFactory questionFactory = new QuestionFactory();
+			
+			ByRefInterpretVisitor iv = new ByRefInterpretVisitor();
+			iv.setXAALConnector(xc);
+			iv.setQuestionFactory(questionFactory);
+			program.jjtAccept(iv, null);
+			System.out.println(Global.getFunction("foo").getParameters().size());
+			System.out.println(Global.getFunction("foo").getSymbolTable().getLocalVariables().size());
+			xc.draw("C:\\Users\\Eric\\Desktop\\RefTest.xaal");
+						for (String line: program.getPseudocode())
+						{
+							System.out.println(line);
+						}
+
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
