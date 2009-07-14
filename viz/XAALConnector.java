9a8f6b5a2f2e40648b3642b28f36659a9e3f9b37
unknown <Eric@.(none)>
2009-07-14 00:30:36 -0500

    more highlighting stuff

--- viz/XAALConnector.java
+++ viz/XAALConnector.java
@@ -2242,7 +2242,8 @@ public class XAALConnector {
 			int snapNum = action.getSnapNum();
 			
 			scripter.reopenSlide(snapNum);
-			for(int i = 0; true; i += 2)
+			int i = 0;
+			for(i = 0; true; i += 4)
 			{
 				Variable nextHighlightedVar = null;
 				String nextHighlightedScope = null;
@@ -2253,7 +2254,7 @@ public class XAALConnector {
 				String tempFaded = fadedScopes.poll();
 				String tempHighScope = highlightScopes.poll();
 				
-				if (tempVar == null || i >=2)
+				if (tempVar == null)
 					break;
 				int tempVarIndex = highlightVarIndexes.poll().intValue();
 				boolean parExists = reopenOrCreatePar(i);
@@ -2286,8 +2287,31 @@ public class XAALConnector {
 				parExists = reopenOrCreatePar(i+1);
 				scripter.addPause(1000);
 				recloseOrEndPar(parExists);
+				
+				//turn off highlighting
+				parExists = reopenOrCreatePar(i+2);
+				scripter.addHide(varHighId);
+				scripter.addHide(scopeHighId);
+				//scripter.add
+				recloseOrEndPar(parExists);
+				
+				parExists = reopenOrCreatePar(i+3);
+				scripter.addPause(250);
+				recloseOrEndPar(parExists);
 			}
 			
+			
+			
+			
+			
+			//TODO start here!
+			
+			/*
+			boolean parExists = reopenOrCreatePar(i);
+			
+			scripter.addChangeStyle(highlightColor, ids)
+			recloseOrEndPar(parExists);
+			*/
 			scripter.recloseSlide();
 		}
 		catch (XAALScripterException e)
